eb245a94d04f79ef9e635e1e9607e348ba9db5f4
Brian <karabinchak.brian@gmail.com>
2021-04-12 21:21:09 -0400

    Made Solver it's own class.  Need to make possible_answers variable private and only accessible via functions.

--- solver/briansolver.py
+++ solver/briansolver.py
@@ -3,30 +3,26 @@ import sys
 sys.path.append('..')
 from components.board import Board
 
-class BrianSolver():
+class Solver():
 	"""
-	This class inherits the Sudoku board class and implements a stack of instructions for soling it, that grows/
-	dynamically changes as numbers are found.
+	This class takes the Sudoku board class as a parameter and implements a stack of instructions for solving it, that grows/
+	dynamically changes as numbers are found.  Abstract  - to help build other solvers faster.
 	Properties:
 		possible_answers: 2D list of sets with possible answers.  sets of size are the correct answer
+		instruction_stack: LifoQueue, of tuples, of the form (function, *args, **kwargs).  ex: (self.do_something,(1,2,3),("keyword_argument_1":"value_1"))
+
+	What YOU eed to implement if you inherit this-
+		self.is_empty()- put some functions, args, kwargs into the stack when you have nothing left to do.
+
 	"""
 	possible_answers = None
+	instructions_performed = []
 
 	def __init__(self, board):
 		self.board = board
 		self.instruction_stack = LifoQueue()
 		self.initialize_possible_answers()
-
-	def round_robin_group(self, group):
-		"""
-		Takes a row and column, and then gets the set of of numbers already existing in that group, and removes
-		from the square based on that.
-		"""
-		print(f"doing group {group}")
-#		group_values = self.get_group_set(row, col)
-#		self.possible_answers[row][col] = self.possible_answers[row][col].difference(group_values)
-
-
+		
 	def initialize_possible_answers(self):
 		"""
 		Before creating any instructions, we first determine possible values for each empty square.
@@ -40,10 +36,10 @@ class BrianSolver():
 		for i in range(0, 9):
 			self.possible_answers[i] = [None, None, None, None, None, None, None, None, None]
 		for row_num, row in enumerate(self.board.board_data):
-			row_values = self.get_row_set(row_num)
+			row_values = self.get_row_set(row_num,skip=True)
 			for col_num, square in enumerate(row):
-				col_values = self.get_col_set(col_num)
-				group_values = self.get_group_set(row_num, col_num)
+#				col_values = self.get_col_set(col_num)
+#				group_values = self.get_group_set(row_num, col_num)
 				if square.value is not None:
 					self.possible_answers[row_num][col_num] = set([square.value])
 				else:
@@ -67,42 +63,92 @@ class BrianSolver():
 					print(f"Found new answer {set_values} for {row_num},{col_num}")
 
 
-	def get_row_set(self, row):
+	def get_row_set(self, row,skip=False):
 		"""
 		Returns a set of all numbers that appear in the row
+		Args:
+			row: int, 0-9
+			skip: bool, if true, dont'write to instructions performed.  only this one needs it
+			for initalization.
 		"""
+		if not skip:
+			self.instructions_performed.append((self.get_row_set,(row,),{}))
 		return set([x.value for x in self.board.board_data[row]])		
 
 	def get_col_set(self, col):
 		"""
 		Returns set of all numbers that appear in the column
 		"""
+		self.instructions_performed.append((self.get_col_set,(col,),{}))
 		return  set([row[col].value for row in self.board.board_data])
 
 	def get_group_set(self, row, col):
 		"""
 		Get set of values in group
 		"""
+		self.instructions_performed.append((self.get_group_set,(row,col),{}))
 		group_num = self.board.board_data[row][col].group
 		return self.board.get_group_value_set(group_num)
+
+	def if_empty():
+		"""
+		What's your baseline instruction when there's nothing obvious to do?
+		"""
+		pass
 
 	def do_next_step(self):
 		"""
 		Creates, and then does, next step into stack.
 		"""
 		if self.instruction_stack.empty():
-			self.instruction_stack.put((self.round_robin_group,(9,),{}))
-			self.instruction_stack.put((self.round_robin_group,(8,),{}))
-			self.instruction_stack.put((self.round_robin_group,(7,),{}))
-			self.instruction_stack.put((self.round_robin_group,(6,),{}))
-			self.instruction_stack.put((self.round_robin_group,(5,),{}))
-			self.instruction_stack.put((self.round_robin_group,(4,),{}))
-			self.instruction_stack.put((self.round_robin_group,(3,),{}))
-			self.instruction_stack.put((self.round_robin_group,(2,),{}))
-			self.instruction_stack.put((self.round_robin_group,(1,),{}))
+			self.if_empty()
 		func, params, keyword_params = self.instruction_stack.get()
 		func(*params, **keyword_params)
 
+	def get_group_reference_numbers(self, group_num):
+		group_row_col_mapping = {
+			1: (slice(0,3), slice(0,3),0,0),
+			2: (slice(0,3), slice(3,6),0,3),
+			3: (slice(0,3), slice(6,9),0,6),
+			4: (slice(3,6), slice(0,3),3,0),
+			5: (slice(3,6), slice(3,6),3,3),
+			6: (slice(3,6), slice(6,9),3,6),
+			7: (slice(6,9), slice(0,3),6,0),
+			8: (slice(6,9), slice(3,6),6,3),
+			9: (slice(6,9), slice(6,9),6,6)
+		}
+		return group_row_col_mapping[group_num]
+	
+	def change_possible_answer(self, row, col, new_set):
+		value_to_write = set(new_set)
+		self.possible_answers[row][col]=new_set
+		self.instructions_performed.append((self.change_possible_answer,(row, col, new_set),{}))
+
+class BrianMethod(Solver):
+	"""
+	My personal method of solving sudoku puzzles in books. Just to get started.
+	Think I have a pretty good method, think it should help dowell.
+	"""
+	def __init__(self, board):
+		super().__init__(board)
+		self.do_next_step()
+
+	def work_on_group(self, number):
+		"""Does my patented brian method on the grou}p"""
+		print(f"Doing work on number {number}")
+		row_slice, col_slice, row_start, col_start = self.get_group_reference_numbers(number)
+		print(f"row nums is {row_slice} col nums is {col_slice}")
+		for cur_row, row in enumerate(self.possible_answers[row_slice], start=row_start):
+			# no need to get row every time getting columng
+			row_set = self.get_row_set(cur_row)
+			for cur_col, current_possible_answers in enumerate(self.possible_answers[cur_row][col_slice], start=col_start):
+				col_set = self.get_col_set(cur_col)
+				print(f"row num {cur_row} col num {cur_col} has {current_possible_answers}")
+		print(f"{self.instructions_performed}")
+
+	def if_empty(self):
+		self.instruction_stack.put((self.work_on_group,(1,),{}))
+
 if __name__ == "__main__":
 	new_game = Board()
-	my_solver = BrianSolver(new_game)
+	my_solver = BrianMethod(new_game)
